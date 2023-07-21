dcdf514b412d286ebeb3ff676718349846323179
THANH LE <thanhlevan0203@gmail.com>
2023-07-20 20:11:23 -0700

    Delete AutoInsuranceMicroservice directory

--- AutoInsuranceMicroservice/src/main/java/com/thanhle/service/DocumentService.java
+++
@@ -1,9 +0,0 @@
-package com.thanhle.service;
-
-import com.thanhle.domain.Document;
-
-public interface DocumentService {
-    Document saveDocument(Document document);
-
-	public Document getDocumentById(Long id);
-}
--- AutoInsuranceMicroservice/src/main/java/com/thanhle/service/PaymentDataService.java
+++
@@ -1,8 +0,0 @@
-package com.thanhle.service;
-
-import com.thanhle.domain.PaymentData;
-
-public interface PaymentDataService {
-	PaymentData savePaymentData(PaymentData paymentData);
-	PaymentData getPaymentDataByPhone(String phone);
-}
